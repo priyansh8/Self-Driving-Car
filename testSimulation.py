cdf2c3452bb9b3d97d5647f4694702c4496c5d55
priyansh8 <43928121+priyansh8@users.noreply.github.com>
2021-05-30 13:57:15 +0530

    Add files via upload

---
+++ testSimulation.py
@@ -0,0 +1,58 @@
+print('Setting Up')
+import pandas as pd
+import numpy as np
+import cv2
+import os
+import socketio
+import eventlet
+from flask import Flask
+from tensorflow.keras.models import load_model
+import base64
+from io import BytesIO
+from PIL import Image
+
+sio = socketio.Server()
+
+app = Flask(__name__)
+maxSpeed = 20
+
+
+def preProcess(img):
+    img = img[60:135, :, :]
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+    img = cv2.GaussianBlur(img, (3, 3), 0)
+    img = cv2.resize(img, (200, 66))
+    img = img / 255
+    return img
+
+
+@sio.on('telemetry')
+def telemetry(sid, data):
+    speed = float(data['speed'])
+    image = Image.open(BytesIO(base64.b64decode(data['image'])))
+    image = np.asarray(image)
+    image = preProcess(image)
+    image = np.array([image])
+    steering = float(model.predict(image))
+    throttle = 1.0 - speed / maxSpeed
+    print('{} {} {}'.format(steering, throttle, speed))
+    sendControl(steering, throttle)
+
+
+@sio.on('connect')
+def connect(sid, environ):
+    print('Connected')
+    sendControl(0, 0)
+
+
+def sendControl(steering, throttle):
+    sio.emit('steer', data={
+        'steering_angle': steering.__str__(),
+        'throttle': throttle.__str__()
+    })
+
+
+if __name__ == '__main__':
+    model = load_model('model.h5')
+    app = socketio.Middleware(sio, app)
+    eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
---
+++ utils.py
@@ -0,0 +1,137 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.utils import shuffle
+import matplotlib.image as mpimg
+from imgaug import augmenters as iaa
+import cv2
+import random
+import os
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Convolution2D,Flatten,Dense
+from tensorflow.keras.optimizers import Adam
+
+def getName(filepath):
+    return filepath.split('\\')[-1]
+def importDatainfo(path):
+    columns = ['Center','Left','Right','Steering','Throttle','Brake','Speed']
+    data = pd.read_csv(os.path.join(path,'driving_log.csv'),names = columns)
+    # print(data.head())
+    # print(data['Center'][0])
+    # print(getName(data['Center'][0]))
+    data['Center'] = data['Center'].apply(getName)
+    # print(data.head())
+    print(data.shape)
+    return data
+
+def balanceData(data, display = True):
+    nBins = 31
+    samplesPerBin = 1000
+    hist, bins = np.histogram(data['Steering'],nBins)
+    # print(bins)
+    if display:
+        center = (bins[:-1] + bins[1:])*0.5
+        # print(center)
+        plt.bar(center,hist,width=0.06)
+        plt.plot((-1,1),(samplesPerBin,samplesPerBin))
+        plt.show()
+
+    removeIndexList = []
+    for j in range(nBins):
+        binDataList = []
+        for i in range(len(data['Steering'])):
+            if(data['Steering'][i]>=bins[j] and data['Steering'][i]<=bins[j+1]):
+                binDataList.append(i)
+        binDataList = shuffle(binDataList)
+        binDataList = binDataList[samplesPerBin:]
+        removeIndexList.extend(binDataList)
+    print("removed Images: ",len(removeIndexList))
+    data.drop(data.index[removeIndexList],inplace = True)
+    print("remaining Images: ", len(data))
+
+    if display:
+        hist, _ = np.histogram(data['Steering'], nBins)
+        plt.bar(center, hist, width=0.06)
+        plt.plot((-1, 1), (samplesPerBin, samplesPerBin))
+        plt.show()
+    return data
+
+def loadData(path, data):
+    imagesPath = []
+    steering = []
+    for i in range(len(data)):
+        indexedData = data.iloc[i]
+        # print(indexedData)
+        imagesPath.append(os.path.join(path,'IMG',indexedData[0]))
+        # print(imagesPath)
+        steering.append(float(indexedData[3]))
+    imagesPath = np.asarray(imagesPath)
+    steering = np.asarray(steering)
+    return imagesPath, steering
+
+def augmentImage(imgPath, steering):
+    img = mpimg.imread(imgPath)
+    # Pan
+    if(np.random.rand()<0.5):
+        pan = iaa.Affine(translate_percent={'x':(-0.1,0.1),'y':(-0.1,0.1)})
+        img = pan.augment_image(img)
+    # Zoom
+    if (np.random.rand() < 0.5):
+        zoom = iaa.Affine(scale=(1,1.2))
+        img = zoom.augment_image(img)
+    # Brightness
+    if (np.random.rand() < 0.5):
+        brightness = iaa.Multiply((0.4,1.2))
+        img = brightness.augment_image(img)
+    # Flip
+    if (np.random.rand() < 0.5):
+        img = cv2.flip(img,1)
+        steering = -steering
+    return img, steering
+
+def preProcessing(img):
+    img = img[60:135,:,:]
+    img=cv2.cvtColor(img,cv2.COLOR_RGB2YUV)
+    img = cv2.GaussianBlur(img,(3,3),0)
+    img=cv2.resize(img,(200,66))
+    img=img/255
+    return img
+
+def batchGen(imagesPath, steeringList, batchSize,traingFlag):
+    while True:
+        imgBatch = []
+        steeringBatch = []
+
+        for i in range(batchSize):
+            index = random.randint(0,len(imagesPath)-1)
+            if(traingFlag):
+                img, steering = augmentImage(imagesPath[index],steeringList[index])
+            else:
+                img = mpimg.imread(imagesPath[index])
+                steering = steeringList[index]
+            img = preProcessing(img)
+            imgBatch.append(img)
+            steeringBatch.append(steering)
+
+        yield (np.asarray(imgBatch),np.asarray(steeringBatch))
+
+def createModel():
+    model = Sequential()
+    model.add(Convolution2D(24,(5,5),(2,2),input_shape=(66,200,3),activation='elu'))
+    model.add(Convolution2D(36, (5, 5), (2, 2), activation='elu'))
+    model.add(Convolution2D(48, (5, 5), (2, 2), activation='elu'))
+    model.add(Convolution2D(64, (3, 3), activation='elu'))
+    model.add(Convolution2D(64, (3, 3), activation='elu'))
+
+    model.add(Flatten())
+    model.add(Dense(100,activation='elu'))
+    model.add(Dense(50, activation='elu'))
+    model.add(Dense(10, activation='elu'))
+    model.add(Dense(1))
+
+    model.compile(Adam(lr=0.0001),loss='mse')
+
+    return model
+
+
